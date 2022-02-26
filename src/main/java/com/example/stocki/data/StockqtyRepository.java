cf693e1784baa34804e96a55c11faf67fbf40060
nagarajasamala <nagarajasamala@gmail.com>
2022-02-26 05:47:16 +0530

    Update StockqtyRepository.java

--- src/main/java/com/example/stocki/data/StockqtyRepository.java
+++ src/main/java/com/example/stocki/data/StockqtyRepository.java
@@ -2,13 +2,14 @@ package com.example.stocki.data;
 
 import org.springframework.data.repository.CrudRepository;
 import com.example.stocki.model.Stockqty;
+import com.example.stocki.model.Users;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
 public interface StockqtyRepository extends CrudRepository<Stockqty, Integer> {
 
-	Iterable<Stockqty> findAllByUserId(Integer id);
+	Iterable<Stockqty> findAllByUsers(Users user);
 	Iterable<Stockqty> findAll();
 
 }
