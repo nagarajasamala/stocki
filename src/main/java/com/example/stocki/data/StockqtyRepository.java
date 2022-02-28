eda6f2751715da5fbf040840af7e1c9e8c9b3ea6
nagarajasamala <nagarajasamala@gmail.com>
2022-02-28 05:38:59 +0530

    Update StockqtyRepository.java

--- src/main/java/com/example/stocki/data/StockqtyRepository.java
+++ src/main/java/com/example/stocki/data/StockqtyRepository.java
@@ -3,6 +3,8 @@ package com.example.stocki.data;
 import org.springframework.data.repository.CrudRepository;
 import com.example.stocki.model.Stockqty;
 import com.example.stocki.model.Users;
+import com.example.stocki.model.Products;
+
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
